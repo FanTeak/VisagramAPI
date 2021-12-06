d48259da6e14d672031f446645b217bfff1b2e3d
Dutchak Bohdan <Dutchak.Bohdan@gmail.com>
2021-12-06 22:48:01 +0200

    Tables build

---
+++ VisagramAPI/Models/SalaryDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisagramAPI.Models
+{
+    public class SalaryDetails
+    {
+        [Key]
+        public long SalaryDetailsId { get; set; }
+
+        public long SalaryPaymentId { get; set; }
+
+        public int SalaryOfferId { get; set; }
+
+        public decimal SalaryOfferPrice { get; set; }
+
+        public float Quantity { get; set; }
+    }
+}
---
+++ VisagramAPI/Models/SalaryPayment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisagramAPI.Models
+{
+    public class SalaryPayment
+    {
+        [Key]
+        public long PaymentId { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string PaymentNumber { get; set; }
+
+        public int StaffId { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string PaymentType { get; set; }
+
+        public decimal Total { get; set; }
+    }
+}
