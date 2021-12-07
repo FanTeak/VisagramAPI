c49c753516832e12e44d7f6dbd4826ad1fa353c2
Dutchak Bohdan <Dutchak.Bohdan@gmail.com>
2021-12-07 02:08:06 +0200

    Entity works, controller salaryPayment done

---
+++ VisagramAPI/Controllers/SalaryPaymentController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisagramAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VisagramAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalaryPaymentController : ControllerBase
+    {
+        private readonly VisagramDbContext _context;
+        public SalaryPaymentController(VisagramDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/Game
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SalaryPayment>>> GetSalaryPayments()
+        {
+            return await _context.SalaryPayments.ToListAsync();
+        }
+
+        //GET: api/Game/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SalaryPayment>> GetSalaryPayment(long id)
+        {
+            var salaryPayment = await _context.SalaryPayments.FindAsync(id);
+            if (salaryPayment == null)
+            {
+                return NotFound();
+            }
+
+            return salaryPayment;
+        }
+
+        // // PUT: api/Game/5
+        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutGame(long id, Game game) {
+        //     if (id != game.GameId) {
+        //         return BadRequest();
+        //     }
+
+        //     _context.Entry(game).State = EntityState.Modified;
+
+        //     foreach (EquipmentUsage item in game.EquipmentUsages) {
+        //         if (item.EquipmentUsageId == 0)
+        //             _context.EquipmentUsages.Add(item);
+        //         else
+        //             _context.Entry(item).State = EntityState.Modified;
+        //     }
+
+        //     foreach (var i in game.DeletedOrderItemIds.Split(',').Where(x => x !="")) {
+        //         OrderDetail y = _context.OrderDetails.Find(Convert.ToInt64(i));
+        //         _context.OrderDetails.Remove(y);
+        //     }
+
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!OrderMasterExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
+
+        //     return NoContent();
+        // }
+
+        // POST: api/Order
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SalaryPayment>> PostSalaryPayment(SalaryPayment salaryPayment)
+        {
+            _context.SalaryPayments.Add(salaryPayment);
+            await _context.SaveChangesAsync();
+            //return CreatedAtAction("GetSalaryPayment", new { id = salaryPayment.PaymentId}, salaryPayment);
+            return CreatedAtAction("GetSalaryPayment", new {id = salaryPayment.PaymentId}, salaryPayment);
+        }
+
+        // DELETE: api/Game/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSalaryPayment(long id)
+        {
+            var salaryPayment = await _context.SalaryPayments.FindAsync(id);
+            if (salaryPayment == null)
+            {
+                return NotFound();
+            }
+
+            _context.SalaryPayments.Remove(salaryPayment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SalaryPaymentExists(long id)
+        {
+            return _context.SalaryPayments.Any(e => e.PaymentId == id);
+        }
+    }
+}
---
+++ VisagramAPI/Migrations/20211206214047_Initial Create.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisagramAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalaryOffers",
+                columns: table => new
+                {
+                    SalaryOfferId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OfferName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    OfferValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalaryOffers", x => x.SalaryOfferId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Staffs",
+                columns: table => new
+                {
+                    StaffId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StaffName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    StaffSurname = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(12)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staffs", x => x.StaffId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalaryPayments",
+                columns: table => new
+                {
+                    PaymentId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PaymentNumber = table.Column<string>(type: "nvarchar(75)", nullable: true),
+                    StaffId = table.Column<int>(type: "int", nullable: false),
+                    PaymentType = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    Total = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalaryPayments", x => x.PaymentId);
+                    table.ForeignKey(
+                        name: "FK_SalaryPayments_Staffs_StaffId",
+                        column: x => x.StaffId,
+                        principalTable: "Staffs",
+                        principalColumn: "StaffId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalaryDetails",
+                columns: table => new
+                {
+                    SalaryDetailsId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SalaryPaymentId = table.Column<long>(type: "bigint", nullable: false),
+                    SalaryOfferId = table.Column<int>(type: "int", nullable: false),
+                    SalaryOfferValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Quantity = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalaryDetails", x => x.SalaryDetailsId);
+                    table.ForeignKey(
+                        name: "FK_SalaryDetails_SalaryOffers_SalaryOfferId",
+                        column: x => x.SalaryOfferId,
+                        principalTable: "SalaryOffers",
+                        principalColumn: "SalaryOfferId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SalaryDetails_SalaryPayments_SalaryPaymentId",
+                        column: x => x.SalaryPaymentId,
+                        principalTable: "SalaryPayments",
+                        principalColumn: "PaymentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalaryDetails_SalaryOfferId",
+                table: "SalaryDetails",
+                column: "SalaryOfferId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalaryDetails_SalaryPaymentId",
+                table: "SalaryDetails",
+                column: "SalaryPaymentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalaryPayments_StaffId",
+                table: "SalaryPayments",
+                column: "StaffId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalaryDetails");
+
+            migrationBuilder.DropTable(
+                name: "SalaryOffers");
+
+            migrationBuilder.DropTable(
+                name: "SalaryPayments");
+
+            migrationBuilder.DropTable(
+                name: "Staffs");
+        }
+    }
+}
